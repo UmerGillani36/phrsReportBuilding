c35aeac8b18d5165017951d2ac2207ca8ca14f79
UmerGillani36 <syedomershahgillani.com>
2022-11-30 02:02:34 -0800

    EOBI Contribution List Done v2

--- src/components/ClientDetails/ClientDetails.js
+++ src/components/ClientDetails/ClientDetails.js
@@ -40,6 +40,12 @@ const ClientDetails = () => {
             })}
         </table>
       </div>
+      <div className={classes.BottomTextDiv}>
+        <p className={classes.BottomText}>
+          EOBI Contribution List - Meezan Bank Limited for the Month of
+          30-Sep-2022
+        </p>
+      </div>
     </page>
   );
 };
